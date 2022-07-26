f59f3908d871570931954cae40d86be8422ea42c
Rick <rickatherton316@gmail.com>
2022-07-26 14:48:51 +0000

    Update profile template and css styles

--- profiles/models.py
+++ profiles/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django_countries.fields import CountryField
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+
+from django_countries.fields import CountryField
 
 
 class UserProfile(models.Model):
@@ -10,7 +11,7 @@ class UserProfile(models.Model):
     A user profile model for maintaining default
     delivery information and order history
     """
-    default_user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_phone_number = models.CharField(max_length=20, null=True, blank=True)
     default_country = CountryField(blank_label='Country *', null=True, blank=True)
     default_postcode = models.CharField(max_length=20, null=True, blank=True)
@@ -22,6 +23,7 @@ class UserProfile(models.Model):
     def __str__(self):
         return self.user.username
 
+
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     """
@@ -29,5 +31,5 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     """
     if created:
         UserProfile.objects.create(user=instance)
-        # Existing users: just save the profile
-        instance.userprofile.save()
+    # Existing users: just save the profile
+    instance.userprofile.save()
